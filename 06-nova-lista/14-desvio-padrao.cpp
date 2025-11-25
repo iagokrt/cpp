8d22719c5f86d75babd7d7f54bb8c7af0735a6b3
Iago Barreto <8128@tcesc.tc.br>
2025-11-25 14:52:32 -0300

    desvio padrao com fns

--- 06-nova-lista/14-desvio-padrao.cpp
+++ 06-nova-lista/14-desvio-padrao.cpp
@@ -5,31 +5,55 @@ using namespace std;
 /**
 Faça um programa em C++ que calcula o desvio padrão de 10 números. Para isso:
 
-Aloque o vetor de teste estaticamente:
-float valores[10] = {1, -8, 9, 255, ... }
-Calcula a média aritmética ( μ  na fórmula ).
-Use a raiz quadrada: sqrt()
-Exiba no final a média e o desvio padrão.
-Desvio padrão: raiz sobre N do somatório do quadrado de cada valor (xi) descontado a média ( μ ).
+    Aloque o vetor de teste estaticamente:
+        float valores[10] = {1, -8, 9, 255, ... }
+    
+    ** Faça uma função que retorna o cálculo da média aritmética ( μ  na fórmula ).
+    ** Faça uma função que retorna o desvio padrão passando como argumentos o vetor, tamanho  e média.
+    
+    Use a raiz quadrada: sqrt()
+    Exiba no final a média e o desvio padrão.
+    Não é permitido o uso do STL (vector, por exemplo).
+    
+    Desvio padrão: raiz sobre N do somatório do quadrado de cada valor (xi) descontado a média ( μ ).
 */
+
+float media_(float valores[10], int tamanho) {
+    float media = 0; 
+    float soma = 0; 
+    int i;
+
+    for (i = 0; i < tamanho; i++) {
+        soma += valores[i];
+    }
+    media = soma / tamanho;
+
+    return media;
+}
+
+float desvio_(float valores[10], int tamanho, float media) {
+    float desvio = 0;
+    int j;
+
+    for (j = 0; j < tamanho; j++) {
+        desvio += (valores[j] - media) * (valores[j] - media);
+    }
+    desvio = sqrt(desvio / tamanho);
+
+    return desvio;
+}
+
 int main() {
     float valores[10] = {1, -8, 9, 255, 2, 4, 6, 8, 10, 12};
 
-    int i,j;
-    float soma = 0;
-    float media = 0;
-    float desvio = 0;
+    int j;
+    float media;
+    float desvio;
 
-    for (i = 0; i < 10; i++) {
-        soma += valores[i];
-    }
-    media = soma / 10;
+    media = media_(valores, 10);
 
-    for (j = 0; j < 10; j++) {
-        desvio += (valores[j] - media) * (valores[j] - media);
-    }
+    desvio = desvio_(valores, 10, media);
 
-    desvio = sqrt(desvio / 10);
     cout << "media:" << media << endl;
     cout << "desvio:" << desvio << endl;
 
