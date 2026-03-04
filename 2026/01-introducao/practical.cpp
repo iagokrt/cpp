009d28ef737e3a294682a531507e6ccd43b48a33
Iago Barreto <8128@tcesc.tc.br>
2026-03-04 16:03:31 -0300

    lista 1 - programacao c++

---
+++ 2026/01-introducao/practical.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+// Mesmo programa da lista 2 ("basic-calculator.cpp")
+// Incluído novamente para referência.
+
+/* modes:
+    1 = "Adição/Soma"
+    2 = "Subtração"
+    3 = "Multiplicação"
+    4 = "Divisão"
+*/
+void calculadora(float a, float b, int op = 1) {
+    switch(op) {
+        case 1:
+            cout << "Soma: " << (a + b) << endl;
+            break;
+        case 2:
+            cout << "Subtracao: " << (a - b) << endl;
+            break;
+        case 3:
+            cout << "Multiplicacao: " << (a * b) << endl;
+            break;
+        case 4:
+            cout << "Divisao: " << (a / b) << endl;
+            break;
+    }
+}
+
+// To-Do: 
+// FOI SOLICITADO NO ITEM 2 a seguinte modificação para o programa:
+// Modifique o programa do exercício (1) para que os valores A e B sejam passados na 
+// inicialização do executável (por meio de argc e argv). (Nota: a função "stof" pode ser 
+// usada para retornar os valores dos argumentos – que estão em formato texto/string – em 
+// valores do tipo float.) 
+
+int main() {
+  float a, b;
+  
+  cout << "Insira o valor A: ";
+  cin >> a;
+  cout << "Insira o valor B: ";
+  cin >> b;
+
+  calculadora(a, b, 1); // 1=SOMA
+  calculadora(a, b, 2); // 2=SUBTRAI
+  calculadora(a, b, 3); // 3=MULTIPLICA
+  calculadora(a, b, 4); // 4=DIVIDE
+
+  return 0;
+}
+
+/**
+    Crie um programa em C++ para enviar a mensagem “Insira o valor A ”. 
+    Após isso, será esperado que o usuário digite um valor em ponto flutuante, ou seja, um número real que possui casas decimais. 
+    Após digitar o valor e pressionar a tecla “ENTER”, uma nova mensagem com o texto “Insira o valor B ” deve aparecer na próxima linha do console, 
+    novamente será esperado que o usuário digite um valor em ponto flutuante. 
+    O programa deve ser capaz de realizar:
+    - a soma, 
+    - subtração, 
+    - multiplicação e 
+    - divisão de A e B, 
+    e mostrar os resultados no console. 
+    Abaixo um exemplo do que deve aparecer no console:
+
+    Insira o valor A 3.5
+    Insira o valor B 2.3
+
+    Soma: 5.8
+    Subtracao: 1.2
+    Multiplicacao: 8.05
+    Divisao: 1.52
+*/
---
+++ 2026/01-introducao/teoria.md
@@ -0,0 +1,39 @@
+# Questões Teóricas
+
+**3) O que é um compilador? É possível utilizar um mesmo compilador para diferentes computadores?**
+
+    R.: Algo que compila??!
+
+**4)  O que é um montador?**
+
+    R.: Algo que monta??!?
+
+**5)  O que é um ligador?**
+
+    R.: Algo que liga??!?
+
+
+**6)  Qual é a diferença entre linguagem assembly, linguagem de alto nível e linguagem de máquina?**
+
+    R.: O quão perto ele fica de linguagem de máquina ou humana?
+
+
+**7)  O que é uma toolchain?**
+
+    R.: Corrente de ferramentas? ele falou disse na aula 01
+
+
+**8)  Qual é a diferença entre um arquivo no formato .cpp e um arquivo no formato .hpp/.h?**
+
+    R.: Um é cabeçalho .h, outro é o arquivo mesmo .cpp
+
+
+**9)  O que é o protótipo de uma função? Porque declarar o protótipo de uma função?** 
+
+    R.: Tipagem ?? sei la pra ficar boas praticas o cabra ja saber o que o metodo espera receber de argumento e oq ele retorna?
+
+
+**10)  Qual é o ponto de entrada de um programa em C++?**
+
+    R.: Arquivo Main
+
