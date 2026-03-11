92dbe9a70adc5194dec0e000cc22e823cef8c87f
Iago Barreto <8128@tcesc.tc.br>
2026-03-11 11:38:35 -0300

    To-do Lista de exercicios n2

---
+++ 2026/02-variaveis/009.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  float a, b;
+  
+  cout << "Insira o valor A: ";
+  cin >> a;
+
+  cout << "Valor: " << a;
+  
+  system("pause");
+  return 0;
+}
