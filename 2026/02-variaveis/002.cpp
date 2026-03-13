db4edc65626ea2d8619a77938e156fdb412240f3
Iago Barreto <8128@tcesc.tc.br>
2026-03-13 18:18:52 -0300

    002 003 resolvidos com args passados na CLI. Estudo do uso do PI

--- 2026/02-variaveis/002.cpp
+++ 2026/02-variaveis/002.cpp
@@ -5,7 +5,7 @@ using namespace std;
   Calcule a área de um retângulo com base em sua largura e altura. A largura e a altura 
 devem ser passadas através da entrada padrão. 
 */
-int main() {
+/*int main() {
   int alt, larg;
   
   cout << "Insira o valor Altura: ";
@@ -16,5 +16,19 @@ int main() {
   cout << "Area: " << (alt * larg) << endl;
   
   // system("pause");
+  return 0;
+} */
+
+int main(int argc, char** argv) {
+  if (argc < 3) {
+    cout << "uso: prg ALTURA LARGURA. \n";
+    return 1;
+  }
+
+  int alt = stoi(argv[1]);
+  int larg = stoi(argv[2]);
+
+  cout << "Area: " << (alt * larg) << endl;
+  
   return 0;
 }
--- 2026/02-variaveis/003.cpp
+++ 2026/02-variaveis/003.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+// #include <numbers> // uso: constexpr double PI = std::numbers::pi;
+#include <cmath> // acos, M_PI
 using namespace std;
 
 /**
   Calcule a área de um círculo com base em seu raio. O raio deve ser passado através da 
 entrada padrão. 
 */
+/*
 int main() {
   constexpr double PI = 3.1415; 
   double r, area;
@@ -17,5 +20,25 @@ int main() {
   cout << "Area: " << area << endl;
   
   // system("pause");
+  return 0;
+} */
+int main(int argc, char** argv) {
+  // constexpr double PI = 3.1415;
+  // constexpr double PI = std::numbers::pi;
+  const double PI = acos(-1);
+
+  double r, area;
+
+  if (argc < 2) {
+    cout << "Insira o valor raio (argumento) ao executar o programa. \n";
+    return 1;
+  }
+
+  r = stod(argv[1]); // stod = string to double
+
+  area = (PI * r * r);
+  
+  cout << "Area: " << area << endl;
+
   return 0;
 }
