e5bdaba81490fb931422e9e79419f6c0c1ffcd51
Iago Barreto <8128@tcesc.tc.br>
2026-03-16 10:43:11 -0300

    int main(int argc, char** argv)

--- 2026/01-introducao/practical.cpp
+++ 2026/01-introducao/practical.cpp
@@ -4,6 +4,28 @@ using namespace std;
 // Mesmo programa da lista 2 ("basic-calculator.cpp")
 // Incluído novamente para referência.
 
+/**
+    Crie um programa em C++ para enviar a mensagem “Insira o valor A ”. 
+    Após isso, será esperado que o usuário digite um valor em ponto flutuante, ou seja, um número real que possui casas decimais. 
+    Após digitar o valor e pressionar a tecla “ENTER”, uma nova mensagem com o texto “Insira o valor B ” deve aparecer na próxima linha do console, 
+    novamente será esperado que o usuário digite um valor em ponto flutuante. 
+    O programa deve ser capaz de realizar:
+    - a soma, 
+    - subtração, 
+    - multiplicação e 
+    - divisão de A e B, 
+    e mostrar os resultados no console. 
+    Abaixo um exemplo do que deve aparecer no console:
+
+    Insira o valor A 3.5
+    Insira o valor B 2.3
+
+    Soma: 5.8
+    Subtracao: 1.2
+    Multiplicacao: 8.05
+    Divisao: 1.52
+*/
+
 /* modes:
     1 = "Adição/Soma"
     2 = "Subtração"
@@ -27,47 +49,45 @@ void calculadora(float a, float b, int op = 1) {
     }
 }
 
-// To-Do: 
-// FOI SOLICITADO NO ITEM 2 a seguinte modificação para o programa:
-// Modifique o programa do exercício (1) para que os valores A e B sejam passados na 
-// inicialização do executável (por meio de argc e argv). (Nota: a função "stof" pode ser 
-// usada para retornar os valores dos argumentos – que estão em formato texto/string – em 
-// valores do tipo float.) 
-
-int main() {
-  float a, b;
+// int main() {
+//   float a, b;
   
-  cout << "Insira o valor A: ";
-  cin >> a;
-  cout << "Insira o valor B: ";
-  cin >> b;
+//   cout << "Insira o valor A: ";
+//   cin >> a;
+//   cout << "Insira o valor B: ";
+//   cin >> b;
 
-  calculadora(a, b, 1); // 1=SOMA
-  calculadora(a, b, 2); // 2=SUBTRAI
-  calculadora(a, b, 3); // 3=MULTIPLICA
-  calculadora(a, b, 4); // 4=DIVIDE
+//   calculadora(a, b, 1); // 1=SOMA
+//   calculadora(a, b, 2); // 2=SUBTRAI
+//   calculadora(a, b, 3); // 3=MULTIPLICA
+//   calculadora(a, b, 4); // 4=DIVIDE
 
-  return 0;
-}
+//   return 0;
+// }
 
 /**
-    Crie um programa em C++ para enviar a mensagem “Insira o valor A ”. 
-    Após isso, será esperado que o usuário digite um valor em ponto flutuante, ou seja, um número real que possui casas decimais. 
-    Após digitar o valor e pressionar a tecla “ENTER”, uma nova mensagem com o texto “Insira o valor B ” deve aparecer na próxima linha do console, 
-    novamente será esperado que o usuário digite um valor em ponto flutuante. 
-    O programa deve ser capaz de realizar:
-    - a soma, 
-    - subtração, 
-    - multiplicação e 
-    - divisão de A e B, 
-    e mostrar os resultados no console. 
-    Abaixo um exemplo do que deve aparecer no console:
+    Item 2)
+    Modifique o programa para que os valores A e B sejam passados na 
+    inicialização do executável (por meio de argc e argv). (Nota: a função "stof" pode ser 
+    usada para retornar os valores dos argumentos – que estão em formato texto/string – em 
+    valores do tipo float.) 
+ */
 
-    Insira o valor A 3.5
-    Insira o valor B 2.3
+int main (int argc, char** argv) {
+    float a, b;
+  
+    if (argc < 3) {
+        cout << "erro. Uso: ./prg A B \n";
+        return 1;
+    }
 
-    Soma: 5.8
-    Subtracao: 1.2
-    Multiplicacao: 8.05
-    Divisao: 1.52
-*/
+    a = stof(argv[1]); 
+    b = stof(argv[2]);
+
+    calculadora(a, b, 1); // 1=SOMA
+    calculadora(a, b, 2); // 2=SUBTRAI
+    calculadora(a, b, 3); // 3=MULTIPLICA
+    calculadora(a, b, 4); // 4=DIVIDE
+
+    return 0;
+}
