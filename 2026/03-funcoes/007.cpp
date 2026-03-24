18db3f2a0a7d8672917cecce5beeb37877c46c34
Iago Barreto <8128@tcesc.tc.br>
2026-03-24 18:10:27 -0300

    muito chata

--- 2026/03-funcoes/007.cpp
+++ 2026/03-funcoes/007.cpp
@@ -4,9 +4,12 @@ using namespace std;
 /**
 7) Elabore uma função que receba como parâmetros dois valores inteiros de 32 bits: a e b.
 A função deve ser capaz de calcular a média aritmética dos valores que pertencerem ao
-intervalo fechado entre a e b. O valor de retorno deve estar em formato de ponto flutuante
-de precisão simples. O cálculo da média deve ser feito de forma crescente e deve ser
-encerrado quando for alcançado o fim do intervalo ou quando o valor zero for alcançado.
+intervalo fechado entre a e b. 
+O valor de retorno deve estar em formato de ponto flutuante de precisão simples.
+
+O cálculo da média deve ser feito de forma crescente e deve ser encerrado 
+quando for alcançado o fim do intervalo ou quando o valor zero for alcançado.
+
 Exemplo: Com entradas a=-3, b=5 e sequência = {-3, -2, -1, 0, 1, 2, 3, 4, 5}, a função deve
 imprimir o valor -2 ([-3 -2 -1]/3). A interação com usuário deve ser feita no console,
 conforme exemplo abaixo:
@@ -18,6 +21,10 @@ Insira o valor em y: 7
 Media = 4.5
 
 */
+
+float funcione(int a, int b) {
+  
+} 
 
 int main() {
   int a, b;
