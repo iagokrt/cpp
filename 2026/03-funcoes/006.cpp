54a790accd3e8c7270a784332b0eb2562439dcbf
Iago Barreto <8128@tcesc.tc.br>
2026-03-24 17:58:22 -0300

    006 com ponteiro

--- 2026/03-funcoes/006.cpp
+++ 2026/03-funcoes/006.cpp
@@ -2,28 +2,39 @@
 using namespace std;
 
 /**
-6) Implemente uma função chamada "CalculaMedia2" que recebe quatro números em ponto
-flutuante de precisão simples. A função deve calcular a média dos três primeiros
-argumentos e guardar o valor no quarto argumento para ser usado fora da função
-(utilizando a sintaxe específica do C++ para parâmetros por referência). Os três valores
-devem ser passados como argumentos para o main (argc e argv). Se o número de valores
-for menor ou maior que três, o programa deve retornar o código de erro. Caso contrário,
-deve imprimir a partir do main no console, o valor da média.
+  6) Implemente uma função chamada "CalculaMedia2" que recebe quatro números em ponto
+  flutuante de precisão simples. 
+  A função deve calcular a média dos três primeiros argumentos 
+  e guardar o valor no quarto argumento para ser usado fora da função
+  (utilizando a sintaxe específica do C++ para parâmetros por referência).
+
+  Os três valores devem ser passados como argumentos para o main (argc e argv). 
+
+  Se o número de valores for menor ou maior que três, o programa deve retornar o código de erro. 
+  Caso contrário,
+  deve imprimir a partir do main no console, o valor da média.
 
 */
 
-int main() {
-  int a, b;
+void CalculaMedia2(float a, float b, float c, float *d) {
+  *d = (a + b + c) / 3;
+}
 
-  cout << "Insira o valor A: ";
-  cin >> a;
-  cout << "Insira o valor B: ";
-  cin >> b;
+int main(int argc, char** argv) {
+  float a, b, c, d;
 
-  cout << "Soma: " << (a + b) << endl;
-  cout << "Subtracao: " << (a - b) << endl;
-  cout << "Multiplicacao: " << (a * b) << endl;
-  cout << "Divisao: " << (a / b) << endl;
+  if (argc <= 3) {
+    cout << "uso: prg A B C. \n";
+    return 1;
+  }
+
+  a = stof(argv[1]);
+  b = stof(argv[2]);
+  c = stof(argv[3]);
+
+  CalculaMedia2(a, b, c, &d);
+
+  cout << d;
 
   return 0;
 }
