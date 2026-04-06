1af807789337ca83255cf7c294819b4a36c03183
Iago Barreto <8128@tcesc.tc.br>
2026-04-06 15:28:55 -0300

    funcoes 10 - todo

---
+++ 2026/03-funcoes/010.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+ 
+using namespace std;
+ 
+/**
+    Crie uma função que receba as velocidades linear (𝑣), em m/s, e angular (𝜔), em rad/s,
+    de um robô móvel do tipo diferencial e retorne as velocidades de atuação nas duas rodas.
+    As equações para o cálculo da cinemática inversa são dadas por:
+    
+    𝑣1 = 𝑣 + 𝑙𝜔
+    𝑣2 = 𝑣 − 𝑣1
+    
+    Onde: 𝑣1 é a velocidade da roda esquerda; 𝑣2 é a velocidade da roda direita e; 𝑙 é a
+    largura do eixo do robô.
+    
+    As velocidades das rodas devem ser repassadas por referência
+    através de dois parâmetros da função.
+    
+    A largura 𝑙 deve ser definida no início do programa,
+    através da entrada padrão acompanhada da mensagem “Insira a largura do eixo: ”.
+    
+    Após
+    isso,
+    
+    o usuário deve passar a máxima velocidade linear e angular do robô, com as
+    seguintes mensagens: “Insira a velocidade linear: ” e;
+    
+    “Insira a velocidade angular: ”.
+    
+    A função deve retornar um de dois possíveis valores enumerados: 'RESULTADO_OK = 0',
+    se calculo ocorreu como esperado, ou 'RESULTADO_FALHO = 1', caso a velocidade
+    linear/angular passadas estejam fora do limite de velocidade do robô. O programa deve
+    apresentar as velocidades das rodas na tela, com as seguintes mensagens: “Velocidade
+    da roda esquerda = ” e “Velocidade da roda direita = ”. Após apresentar as velocidades
+    das rodas, o programa deve esperar 1 segundo (use a biblioteca “chrono” e “thread” do
+    C++) e pedir novamente as velocidades linear e angular. O programa irá encerrar apenas
+    quando forçado na IDE.
+    Exemplo de uso da biblioteca “chrono” e “thread” para gerar um atraso de 1s:
+    #include <chrono>
+    #include <thread>
+    ...
+    while ( true )
+    {
+    ...
+    this_thread::sleep_for(1000ms); //espera 1 segundo
+    }
+ 
+*/
+ 
+void velocity(int &v, int &w, int largura) {
+    //int v1 = v +     
+}
+  
+ 
+int main() {
+  int l = 0;
+  int v = 0;
+  int w = 0;
+  
+  v1 = 0;
+  v2 = 0;
+  
+  float result = 0;
+ 
+  cout << "Insira a largura do eixo: ";
+  cin >> l;
+ 
+  cout << "Insira a velocidade linear: ";
+  cin >> v;
+ 
+  cout << "Insira a velocidade angular: ";
+  cin >> w;
+ 
+  velocity(v, w, l);
+ 
+ 
+  
+  return 0;
+}
