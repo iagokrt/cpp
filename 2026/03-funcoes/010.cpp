b52c86b407c3cddaca014440b2e2ef5a91540f01
Iago Barreto <8128@tcesc.tc.br>
2026-04-06 15:51:48 -0300

    10 exercicio resolvido. nojo desse aqui

--- 2026/03-funcoes/010.cpp
+++ 2026/03-funcoes/010.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
- 
+#include <cmath>
+
 using namespace std;
  
 /**
@@ -31,12 +32,15 @@ using namespace std;
     
     A função deve retornar um de dois possíveis valores enumerados: 'RESULTADO_OK = 0',
     se calculo ocorreu como esperado, ou 'RESULTADO_FALHO = 1', caso a velocidade
-    linear/angular passadas estejam fora do limite de velocidade do robô. O programa deve
-    apresentar as velocidades das rodas na tela, com as seguintes mensagens: “Velocidade
-    da roda esquerda = ” e “Velocidade da roda direita = ”. Após apresentar as velocidades
-    das rodas, o programa deve esperar 1 segundo (use a biblioteca “chrono” e “thread” do
-    C++) e pedir novamente as velocidades linear e angular. O programa irá encerrar apenas
-    quando forçado na IDE.
+    linear/angular passadas estejam fora do limite de velocidade do robô. 
+    
+    O programa deve apresentar as velocidades das rodas na tela, com as seguintes mensagens: 
+    “Velocidade da roda esquerda = ” e “Velocidade da roda direita = ”.
+    
+    Após apresentar as velocidades das rodas, o programa deve esperar 1 segundo (use a biblioteca “chrono” e “thread” do
+    C++) e pedir novamente as velocidades linear e angular.
+    O programa irá encerrar apenas quando forçado na IDE.
+
     Exemplo de uso da biblioteca “chrono” e “thread” para gerar um atraso de 1s:
     #include <chrono>
     #include <thread>
@@ -49,33 +53,58 @@ using namespace std;
  
 */
  
-void velocity(int &v, int &w, int largura) {
-    //int v1 = v +     
+
+enum Res { RESULTADO_OK = 0, RESULTADO_FALHO = 1 };
+
+Res velocity(int &v, int &w, int l, int &v1, int &v2) {
+    const double MAX = 100.0;
+    
+    if (abs(v) > MAX || abs(w) > MAX) {
+        return RESULTADO_FALHO;
+    }
+
+    v1 = v + l*w;
+    v2 = v - v1;
+    
+    // cout << "Velocidade da roda esquerda = " << v1 << endl;
+    // cout << "Velocidade da roda direita = " << v2 << endl;
+    
+    return RESULTADO_OK;
+
 }
-  
  
 int main() {
-  int l = 0;
   int v = 0;
   int w = 0;
+  int l = 0;
   
-  v1 = 0;
-  v2 = 0;
+  int v1 = 0;
+  int v2 = 0;
   
   float result = 0;
  
   cout << "Insira a largura do eixo: ";
   cin >> l;
  
-  cout << "Insira a velocidade linear: ";
-  cin >> v;
- 
-  cout << "Insira a velocidade angular: ";
-  cin >> w;
- 
-  velocity(v, w, l);
- 
- 
+  while(true) {
+    cout << "Insira a velocidade linear: ";
+    cin >> v;
+    
+    cout << "Insira a velocidade angular: ";
+    cin >> w;
+
+    Res result = velocity(v, w, l, v1, v2);
+
+    if (result == RESULTADO_OK) {
+        cout << "Velocidade da roda esquerda = " << v1 << endl;
+        cout << "Velocidade da roda direita = " << v2 << endl;
+    } else {
+        cout << "Velocidade linear/angular fora do limite do robo." << endl;
+    }
+
+    this_thread::sleep_for(1000ms);
+
+  }
   
   return 0;
 }
