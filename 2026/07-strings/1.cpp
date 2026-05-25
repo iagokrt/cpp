383005e48ba9f76914da378a13bb0dc59d2c029e
Iago Barreto <8128@tcesc.tc.br>
2026-05-25 17:41:28 -0300

    strings -1

--- 2026/07-strings/1.cpp
+++ 2026/07-strings/1.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstring>
+
 using namespace std;
  
 /* Lista 7 - Strings */
@@ -10,109 +12,64 @@ using namespace std;
  
 // 1 - Escreva um programa que recebe duas strings do usuário e concatena-as em uma terceira string. Em seguida, exiba o resultado.
  
-// 2 - Crie um programa que receba do terminal uma string e conte o número de caracteres (sem contar espaços em branco e o caractere nulo) e imprimima o resultado.
- 
-// 3 - Implemente uma função para inverter uma string. O programa deve aceitar uma string como entrada, inverter e exibir o resultado.
- 
-// 4 - Escreva um programa que compara duas strings e determina se são iguais ou diferentes.
- 
-// 5 - Crie um programa que substitui todas as ocorrências de um caractere específico em uma string por outro caractere fornecido pelo usuário.
- 
-// 6 - Desenvolva um programa que conta o número de palavras em uma string. Considere que as palavras são separadas por espaços em branco
- 
-// 7 - Implemente uma função que verifica se uma string é um palíndromo (lê-se da mesma forma de trás para frente).
- 
-// 8 - Crie um programa que recebe uma string e remove todos os espaços em branco dela
- 
-// 9 - Escreva um programa que recebe uma string e exibe cada palavra em uma nova linha.
- 
-// 10 - Implemente um programa que converte todas as letras maiúsculas de uma string em minúsculas e vice-versa.
- 
-// 11 - Crie um programa que conta o número de vogais e consoantes em uma string.
- 
-// 12 - Desenvolva um programa que verifica se uma dada string é um endereço de e-mail válido. Considere pelo menos a presença de "@" e ".".
- 
 string concat(string a, string b) {
   return a + b;
 }
- 
-string reverse(string a) {
-  string reverse = "";
-  
-  for (int i = a.size() - 1; i >= 0; i--) {
-    reverse += a[i];
+
+void concat_cs(char a[], char b[], char c[]) {
+  strcpy(c, a);
+  strcat(c, b);
+}
+
+void concat_memo(char a[], char b[], char c[]) {
+  int i = 0;
+
+  while (a[i] != '\0') {
+    c[i] = a[i]; // copiar primeira string para c
+    i++;
   }
-  
-  return reverse;
-}
- 
-bool compare(string &a, string &b) {
-  //if (a == b) return true;
-  return (a == b);
-}
- 
-string replace(string &a, char cc, char r) {
- 
-  int a_size = a.size();
-  
-    for (int i = 0; i < a_size; i++) {
-      if (a[i] == cc) {
-        a[i] = r;  // substitui
-      }
-    }
-  
-  return a;
+  int j = 0; // salva onde 'parou de contar'
+
+  while (b[j] != '\0') {
+    c[i] = b[j];
+    i++;
+    j++;
+  }
+  c[i] = '\0'; // ultimo item do array, 'fechar' a string.
+
 }
 
 int main(int argc, char** argv) {
   string a, b, c;
   
-  char cc, r;
-  
-  /* // QUESTAO 1
+  // std::string
   cout << "string a: ";
   cin >> a;
+
   cout << "string b: ";
   cin >> b;
-  cout << "Concat: " << concat(a, b) << endl; */
   
-  /* // QUESTAO 2
-  string words;
-  int count = 0;
-  if (argc < 2) {
-    return 1;
-  }
-  for (int i = 0; i < argc; i++) {
-    cout << "i: " << i << endl;
-    if (i == 0) continue;
-    string word = argv[i];
-    int word_CharCount = word.size();
-    count += word_CharCount;
-  }
-  cout << "char count: " << count << endl; */
-  
-  /* // QUESTAO 3
-  cout << "string a: ";
-  cin >> a;
-  cout << "reverse a: " << reverse(a) << endl; */
-  
-  /* // QUESTAO 4
-  cout << "string a: ";
-  cin >> a;
-  cout << "string b: ";
-  cin >> b;
-  cout << "Iguais?: " << compare(a, b) << endl; */
-  
-  /* // QUESTAO 5
-  cout << "string a: ";
-  cin >> a;
-  cout << "troque cc: ";
-  cin >> cc;
-  cout << "por r: ";
-  cin >> r;
-  
-  cout << "replaced: " << replace(a, cc, r) << endl; */
+  c = a + b;
+  cout << "Concat (std::string): " << c << endl;
+
+  // <cstring>
+  char as[50];
+  char bs[50];
+  char cs[100];
+  char cs_2[100];
+
+  cout << "char as: ";
+  cin >> as;
+  cout << "char bs: ";
+  cin >> bs;
+
+  concat_cs(as, bs, cs);
+  cout << "Concat (<cstring>): " << cs << endl;
  
- 
+  // strings na memoria
+  concat_memo(as, bs, cs_2);
+  cout << "Concat (char[]): " << cs_2 << endl;
+
+
   return 0;
 }
