b26d70ac3bb3e92db9a3388e4259cb7f5f80a00c
Iago Barreto <8128@tcesc.tc.br>
2026-05-27 17:53:55 -0300

    rmv brancoses

--- 2026/07-strings/8.cpp
+++ 2026/07-strings/8.cpp
@@ -8,7 +8,35 @@ using namespace std;
 // Implementacao 2 - biblioteca <cstring>
 // Implementacao 3 - strings na memoria
 
+void remove_blanks(char s[]) {
+  int size = 0;
+
+  while (s[size] != '\0') size++;
+
+  for (int i = 0; i < size; i++) {
+    if (s[i] == ' ') {
+      // s[i] = ''; não existe char vazio na memoria, impossivel dessa forma..
+      // puxar char depois do espaço
+      for (int j = i; j < size; j++) {
+        s[j] = s[j + 1];
+      }
+      size--;
+      i--;
+
+    }
+  }
+  // return s;
+}
+
+
 int main() {
-  cout << "Hello World!";
+  char s[100];
+
+  cout << "Fase: ";
+  cin.getline(s, 100);
+
+  remove_blanks(s);
+  cout << s << endl;
+
   return 0;
 }
