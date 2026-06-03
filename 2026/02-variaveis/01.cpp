62fc060ef03610a732c63c29a1c41b5b21e6c535
Iago Barreto <8128@tcesc.tc.br>
2026-06-03 18:08:59 -0300

    tt

--- 2026/02-variaveis/01.cpp
+++ 2026/02-variaveis/01.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
 using namespace std;
 
-/**
-  Peça ao usuário para inserir dois números inteiros e exiba o resultado da adição, 
-subtração, multiplicação e divisão desses números.
-*/
+// 1
+// Peça ao usuário para inserir dois números inteiros e exiba o resultado da adição, 
+// subtração, multiplicação e divisão desses números. 
 
 int main() {
-  return 0;
+    int a, b;
+    
+    cout << "Digite A:";
+    cin >> a;
+    cout << "Digite B:";
+    cin >> b;
+
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+    cout << a / b << endl;
+
+    return 0;
 }
+
+// int calc_soma(int a, int b) {
+
+// }
+// int calc_sub(int a, int b) {
+
+// }
+// int calc_mult(int a, int b) {
+
+// }
+// int calc_div(int a, int b) {
+
+// }
+// int res_1, res_2, res_3, res_4;
+// res_1 = calc_soma(a, b);
+// res_2 = calc_sub(a, b);
+// res_3 = calc_mult(a, b);
+// res_4 = calc_div(a, b);
