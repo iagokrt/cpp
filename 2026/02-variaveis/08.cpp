0fbff05e0d0b7f2b8b1e41e7f3dd398eea62ce61
Iago Barreto <8128@tcesc.tc.br>
2026-06-16 17:37:22 -0300

    boolalpha

--- 2026/02-variaveis/08.cpp
+++ 2026/02-variaveis/08.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
+
 using namespace std;
+
 
 /**
  Solicite ao usuário sua idade e determine se ele é considerado adulto (idade maior ou igual 
   a 18) ou não. Se for adulto, coloque true na saída padrão, caso contrário coloque false.
 */
 
-
+bool maioridade(int idade) {
+  return (idade >= 18);
+}
 
 int main() {
 
+  int idade = 0;
+
   cout << "Digite idade" << endl;
+  cin >> idade;
+
+  cout << boolalpha << maioridade(idade) << endl;
 
   return 0;
 }
